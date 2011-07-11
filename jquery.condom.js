8931cf1b22071428902b8f624d2fdf1ab989d590
Mario "Kuroir" Ricalde <kuroir@gmail.com>
2011-07-11 09:20:42 -0700

    Give you a way to convert any pre-made jqeury plugin into a namespaced plugin by adding .ns('') before the .fn declaration. Ex. $.ns('my').fn.myPlugin = function() {}

--- jquery.condom.js
+++ jquery.condom.js
@@ -1,6 +1,6 @@
 /* 
  * jQuery Condom (Use namespaces to protect your global integrity.) 
- * Version 0.0.2
+ * Version 0.0.3
  * 
  * Copyright (c) 2011 Mario "Kuroir" Ricalde (http://kuroir.com)  
  *   & Micha Niskin (micha@thinkminimo.com) 
@@ -15,8 +15,12 @@
 
     // Get reference to a namespaced jQ object
     function nsfun(selector, context) {
+      console.log(selector);
       return $(selector, context).ns(ns);
     }
+    
+    // Allows you to add methods ala jQuery.fn (useful to namespace premade plugins)
+    nsfun.fn = methods[ns];
 
     // Add a method.
     nsfun.add = function(fname, fn) { 
